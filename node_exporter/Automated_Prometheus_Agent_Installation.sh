b83f81aa9ef1e43ad675b9938ebfe3870f05c23a
maayangiladofer <maayangiladofer@gmail.com>
2024-03-18 21:01:13 +0200

    added already running check, fixed while loop and added cleanup

--- node_exporter/Automated_Prometheus_Agent_Installation.sh
+++ node_exporter/Automated_Prometheus_Agent_Installation.sh
@@ -15,18 +15,20 @@ else
 	echo "Successfully downloaded the exporter latest version from GitHub!"
 fi
 
-# Extracting the compressed archive
-tar xvfz ${exporter_type}-latest.tar.gz
+# Extracting the compressed archive and saving version number as variable
+tar xvfz ${exporter_type}-latest.tar.gz 
+latest_ver_downloaded=$(ls -d node_exporter-*/ | head -n 1 | awk -F'[-.]' '{print $2 "." $3 "." $4}')
+ver_running=$(${exporter_type} --version 2>&1 | grep 'version' | head -n -1  | awk '{print $3}')
 
+if [ ! "$latest_ver_downloaded" == "$ver_running" ]; then
+	# Copy the Exporter binary to the appropriate location and set ownership:
+	cp -rf ${exporter_type}*.linux-amd64/${exporter_type} /usr/local/bin/
+	chown $exporter_type:$exporter_type  /usr/local/bin/$exporter_type
 
-# Copy the Exporter binary to the appropriate location and set ownership:
-cp ${exporter_type}*.linux-amd64/${exporter_type} /usr/local/bin/
-chown $exporter_type:$exporter_type  /usr/local/bin/$exporter_type
+	if [ "$exporter_type" == "node_exporter" ] && [ ! -e "/etc/systemd/system/${exporter_type}.service"  ] ; then
 
-if [ "$exporter_type" == "node_exporter" ] && [ ! -e "/etc/systemd/system/${exporter_type}.service"  ] ; then
-
-        # Create a systemd unit file for the node_exporter and define the Node Exporter service in the unit file:
-        cat <<EOF > /etc/systemd/system/${exporter_type}.service
+		# Create a systemd unit file for the node_exporter and define the Node Exporter service in the unit file:
+		cat <<EOF > /etc/systemd/system/${exporter_type}.service
 [Unit]
 Description=Prometheus Node Exporter Service
 Wants=network-online.target
@@ -44,15 +46,15 @@ EOF
 
 
 
-elif [ "$exporter_type" == "snmp_exporter" ] && [ ! -e "/etc/systemd/system/${exporter_type}.service"  ]; then
+	elif [ "$exporter_type" == "snmp_exporter" ] && [ ! -e "/etc/systemd/system/${exporter_type}.service"  ]; then
 
 
-       # Copy configuration file 'snmp.yaml', file that configures SNMP targets and metrics for monitoring with the snmp_exporter, also set ownership:
-        cp ${exporter_type}*.linux-amd64/snmp.yml /usr/local/bin/
-        chown $exporter_type:$exporter_type  /usr/local/bin/snmp.yml
+	       # Copy configuration file 'snmp.yaml', file that configures SNMP targets and metrics for monitoring with the snmp_exporter, also set ownership:
+		cp -f ${exporter_type}*.linux-amd64/snmp.yml /usr/local/bin/
+		chown $exporter_type:$exporter_type  /usr/local/bin/snmp.yml
 
-        # Create a systemd unit file for the snmp_exporter and define the SNMP Exporter service in the unit file:
-        cat <<EOF > /etc/systemd/system/${exporter_type}.service
+		# Create a systemd unit file for the snmp_exporter and define the SNMP Exporter service in the unit file:
+		cat <<EOF > /etc/systemd/system/${exporter_type}.service
 [Unit]
 Description=Prometheus SNMP Exporter Service
 After=network.target
@@ -68,25 +70,34 @@ WantedBy=multi-user.target
 EOF
 
 
+	fi
+else
+	echo "Service is already up and running at the latest version"
+	service_already_up=true
+
+fi
+
+# Reload system configuration if service is already installed
+if [ ! $service_already_up ]; then
+	systemctl daemon-reload
 fi
 
 # Start and enable the $(exporter_type) service:
-systemctl daemon-reload
 systemctl enable --now ${exporter_type}.service
 
-# curl the exporter ip and port and grab the http response to check the exporter is up and running
-if [ "$exporter_type" == "node_exporter" ]; then
-	
-	response_code=$(curl -s -o /dev/null -w "%{http_code}" "localhost:9100")
-elif [ "$exporter_type" == "snmp_exporter" ]; then
-
-	response_code=$(curl -s -o /dev/null -w "%{http_code}" "localhost:9116")
-fi
-
-# If up and running, send success message, if failed also send message and exit with failure
-
-int1=10
+int1=20
 while [ $int1 -gt 0 ]; do
+
+	# curl the exporter ip and port and grab the http response to check the exporter is up and running
+	if [ "$exporter_type" == "node_exporter" ]; then
+		
+		response_code=$(curl -s -o /dev/null -w "%{http_code}" "localhost:9100")
+	elif [ "$exporter_type" == "snmp_exporter" ]; then
+
+		response_code=$(curl -s -o /dev/null -w "%{http_code}" "localhost:9116")
+	fi
+
+	# If up and running, send success message, if failed also send message and exit with failure
 
 	if [ "$response_code" -eq 200 ]; then
 
@@ -103,4 +114,5 @@ while [ $int1 -gt 0 ]; do
 done
 
 
-
+# Cleanup files downloaded and extracted
+  rm -rf ${exporter_type}-latest.tar.gz ${exporter_type}-latest ${exporter_type}*.linux-amd64
