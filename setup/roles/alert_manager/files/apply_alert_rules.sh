99ae259c9c67a64d3e98946c522d75491d180d5f
maayangiladofer <maayangiladofer@gmail.com>
2024-03-23 11:48:58 +0200

    added alertmanager, configured alerts to be inserted to configMap (not working atm)

--- setup/roles/alert_manager/files/apply_alert_rules.sh
+++ setup/roles/alert_manager/files/apply_alert_rules.sh
@@ -1,29 +1,26 @@
-#!/bin/bash
+#!/bin/bash -x 
 
 PROMETHEUS_HELM_CHART_NAME="prometheus"
 HELM_CHART_REPO="prometheus-community/prometheus"
-CUSTOM_RULE_CONFIG_PATH="generate_rule_file.yaml"
-
+ALERTING_RULES=$(for file in *.yml; do cat "$file"; echo; done)
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+CUSTOM_RULE_CONFIG_PATH="$SCRIPT_DIR/alerting_rules.yml"
+CONFIG_MAP=:
 
 # Create an empty custom scrape configuration file
 function create_custom_rule_file() {
-        > "$CUSTOM_RULE_CONFIG_PATH"
-	cat "groups:" >> generate_rule_file.yaml
+	cat <<EOF > $CUSTOM_RULE_CONFIG_PATH
+groups:
+$(echo "$ALERTING_RULES" | sed 's/^/  /')
+EOF
 }
 
-function combine_rules_to_custome_rule_file() {
-	for file in files/*.yml; do
-        	if [ -f "$file" ]; then
-        		cat "$file" >> "$CUSTOM_RULE_CONFIG_PATH"
-        	fi
-        done
-}
 
 # Upgrade the Prometheus Helm chart with custom configurations
 function upgrade_helm_chart() {
         helm upgrade "$PROMETHEUS_HELM_CHART_NAME" "$HELM_CHART_REPO" \
                 --reuse-values \
-                --set-file serverFiles.alerting_rules.yml="$CUSTOM_RULE_CONFIG_PATH"
+                -f "$CUSTOM_RULE_CONFIG_PATH"
 }
 
 # Delete the custom scrape configuration file
@@ -38,17 +35,13 @@ function delete_custom_rule_file() {
 # Create the custom scraping configuration file
 create_custom_rule_file
 
-# Add all rules to a main yaml file
-combine_rules_to_custome_rule_file
 
 # Upgrade the Helm chart with the custom scrape configuration
-upgrade_helm_chart &> /dev/null && echo "Helm Chart was succesfully updated!" || echo "ERROR: Failed to update Helm chart."
+#upgrade_helm_chart && echo "Helm Chart was succesfully updated!" || echo "ERROR: Failed to update Helm chart."
 
 
 # Delete the temporary custom scrape configuration file
-delete_custom_rule_file
+#delete_custom_rule_file
 
 
-# Restart Prometheus to apply the new configuration
-killall -HUP prometheus
 
